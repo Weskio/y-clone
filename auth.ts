caee44800c61ee875766106f3f9f7776e34898ad
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2025-01-24 01:51:23 +0000

    chore: author auth

--- auth.ts
+++ auth.ts
@@ -1,6 +1,9 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
- 
+import { client } from "./sanity/lib/client";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/queries";
+import { writeClient } from "./sanity/lib/write-client";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GitHubProvider({
@@ -8,5 +11,30 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientSecret: process.env.AUTH_GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({
+      user: { email, image },
+      account,
+      profile: { id, name, login, bio } = {},
+    }) {
+      const existingUser = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+        id: id,
+      });
+
+      if (!existingUser) {
+        await writeClient.create({
+          _type: "author",
+          id,
+          name,
+          username: login,
+          email,
+          image,
+          bio: bio || "",
+        });
+      }
+
+      return true;
+    },
+  },
   secret: process.env.AUTH_SECRET,
-})
+});
