3f566c2254f8c81ab67dafa2b6c5bb4f5e19bba1
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2025-01-24 13:38:53 +0000

    chore: promise type update

--- app/(dashboard)/dashboard/users/[id]/page.tsx
+++ app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const page = async ({ params }: { params: { id: string } }) => {
-  const {id} = await params;
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
 
   return <div>Single User {id}</div>;
 };
