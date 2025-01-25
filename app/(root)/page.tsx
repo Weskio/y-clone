79c8978eed6f7103d9b9b142778a0c9b45b4f3f1
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2025-01-25 04:12:42 +0000

    fix: es lint issues pt.2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,3 @@
-import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
 import StartupCard from "@/components/StartupCard";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
