568aba94c0e153b2dca850b5dc83087060da030e
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2025-03-12 14:43:15 +0000

    chore: server error fixed

--- sanity/lib/write-client.ts
+++ sanity/lib/write-client.ts
@@ -3,9 +3,6 @@ import "server-only";
 import { createClient } from "next-sanity";
 
 import { apiVersion, dataset, projectId, token } from "../env";
-import { auth } from "@/auth";
-
-(async () => await auth())();
 
 export const writeClient = createClient({
   projectId,
